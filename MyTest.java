eff145e70fb48d4d9edb3d1ea9cc58af8ec5cf68
uko31 <m.gregoriades@gmail.com>
2021-05-20 16:32:33 +0200

    first attempt

---
+++ MyTest.java
@@ -0,0 +1,13 @@
+class MyTest {
+
+	public static void main(String[] args) {
+		System.out.println("this is a test");
+		if ( args.length > 0 ) {
+			System.out.println("\targs provided.");
+			for (int i=0; i<args.length; i++) {
+				System.out.println("\t\t" + args[i]);
+			}
+		}
+	}
+
+}
